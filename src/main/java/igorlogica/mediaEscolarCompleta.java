d90c4f7315f9bfed630f1376f13f5aeac9a4e348
igormedeirospe <igormedeiros407@gmail.com>
2024-06-19 08:22:28 -0300

    commit

--- src/main/java/igorlogica/mediaEscolarCompleta.java
+++ src/main/java/igorlogica/mediaEscolarCompleta.java
@@ -1,2 +1,32 @@
-package igorlogica;public class mediaEscolarCompleta {
+package igorlogica;
+
+public class mediaEscolarCompleta {
+    public static void main(String[] args) {
+
+        java.util.Scanner objetoNota = new java.util.Scanner(System.in);
+        double nota1, nota2, nota3, nota4, media;
+
+        System.out.println("digite sua nota1");
+        nota1 = objetoNota.nextDouble();
+
+        System.out.println("digite sua nota2");
+        nota2 = objetoNota.nextDouble();
+
+        System.out.println("digite sua nota3");
+        nota3 = objetoNota.nextDouble();
+
+        System.out.println("digite sua nota4");
+        nota4 = objetoNota.nextDouble();
+
+        media = (nota1 + nota2 + nota3 + nota4) /4;
+
+        if(media >=7){
+            System.out.println("parabens voce atingiu nota suficiente de media " + media);
+        }else if(media >=4){
+            System.out.println("voce esta em recuperacao pois atingiu media " + media);
+        }else{
+            System.out.println("reprovado");
+        }
+
+    }
 }
--- src/main/java/igorlogica/scanner.java
+++ src/main/java/igorlogica/scanner.java
@@ -1,2 +1,38 @@
-package igorlogica;public class scanner {
+package igorlogica;
+
+public class scanner {
+    public static void main (String[] args){
+
+
+        //instancia objeto da classe
+        //System in - lê aa informações do teclado
+        //Scanner - Objeto da classe Scanner
+        java.util.Scanner objetoNome = new java.util.Scanner(System.in);
+
+        System.out.println("digite seu nome: ");
+
+        //ler oque o usuario digitar e armazenar na variavel nome
+        String nome = objetoNome.nextLine();
+        //------------------------------------------
+
+        System.out.println("digite sua idade: ");
+        //ler oque o usuario digitar e armazenar na variavel nome
+        int idade = objetoNome.nextInt();
+
+        System.out.println("nome digitado: " + nome );
+        System.out.println("idade digitada: " + idade );
+        System.out.println(nome + " tem " + idade + " anos" );
+        /*
+
+
+
+
+
+
+
+
+
+
+         */
+    }
 }
